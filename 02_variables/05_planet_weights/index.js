bd7ab4a27c25ef375f67f88dcfe31da25d224901
Arbajxml <arbaj334alam@gmail.com>
2025-06-10 22:38:03 +0530

    Added Planet weghts

---
+++ 02_variables/05_planet_weights/index.js
@@ -0,0 +1,9 @@
+// Write code below 💖
+
+let earth_weight = 132;
+
+const destination_weigth = earth_weight * 0.38;
+
+console.log("Your weight on earth is ", earth_weight, " pounds.")
+
+console.log("Your weight on mars is ", destination_weigth, " pounds.")
