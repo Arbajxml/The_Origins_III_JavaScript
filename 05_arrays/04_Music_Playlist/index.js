b0e63c30395faebc81e336e987985f063a68246b
Arbajxml <arbaj334alam@gmail.com>
2025-06-13 20:34:33 +0530

    Added Arrays

---
+++ 05_arrays/04_Music_Playlist/index.js
@@ -0,0 +1,23 @@
+// Write code below 💖
+
+const musicPlaylist = [
+  "Tom Sawyer",
+  "Sabotage",
+  "I Wanna Dance With Somebody",
+  "Don't Speak",
+  "Bulls On Parade",
+  "Thriller",
+  "The Breaks",
+  "Brick",
+  "Aeroplane Over the Sea",
+  "Tubthumping"
+];
+
+musicPlaylist.shift();
+musicPlaylist.pop();
+
+musicPlaylist.push("ShutDown");
+musicPlaylist.unshift("Farebi");
+musicPlaylist.unshift("Perfect Pair");
+
+console.log(musicPlaylist);
